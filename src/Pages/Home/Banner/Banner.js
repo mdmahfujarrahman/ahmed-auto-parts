1dd3a6bba296f0cdc06fec92eaaf944c57c86fb2
mdmahfujarrahmane <ahmedmahfuj28@gmail.comm>
2022-05-25 17:27:41 +0600

    extra section added

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -11,13 +11,15 @@ const Banner = () => {
                 <div className="flex flex-col md:flex-row justify-center items-center h-screen">
                     <div className="text-white text-center md:text-left animate__animated animate__fadeInTopLeft">
                         <h1 className="text-5xl my-6">
-                            Auto Parts Online Store
+                            Auto Parts Manufacturer
                         </h1>
                         <p className="my-4">
                             Special offer for auto parts for electric cars and
                             hybrid vehicles. To select the spare part you need.
                         </p>
-                        <button className="btn btn-primary my-4">Buy Now</button>
+                        <button className="btn btn-primary my-4">
+                            Buy Now
+                        </button>
                     </div>
                     <div className="w-11/12 animate__animated animate__fadeInRightBig">
                         <img className="mt-8 md:mt-0" src={car} alt="" />
--- src/Pages/Home/OurFactory/OurFactory.js
+++ src/Pages/Home/OurFactory/OurFactory.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import factory from '../../../asset/factory.jpg';
 
 const OurFactory = () => {
     return (
-        <div>
-            
-        </div>
+        <section>
+            <div className="hero min-h-screen bg-base-200">
+                <div className="hero-content p-0 flex-col lg:flex-row">
+                    <img
+                        src={factory}
+                        alt="factory"
+                        className="max-w-sm lg:max-w-lg rounded-lg shadow-2xl"
+                    />
+                    <div className="text-center lg:text-left ml-0 lg:ml-10">
+                        <h1 className="text-5xl font-bold">Our Factory</h1>
+                        <p className="py-6">
+                            Ahmed Auto Parts offers a unique combination of
+                            know-how in plastics moulding technologies for
+                            clients in the automotive industry worldwide. Our
+                            research and development facilities, design offices
+                            and our practiced sales team provide us with
+                            expertise that allows us to offer a full range of
+                            plastics based automotive solutions........
+                        </p>
+                        <button className="btn btn-primary">See More</button>
+                    </div>
+                </div>
+            </div>
+        </section>
     );
 };
 
