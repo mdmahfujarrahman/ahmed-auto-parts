6b1edfcc9e4762c721b7040fcc1d8beb5fdb0491
mdmahfujarrahmane <ahmedmahfuj28@gmail.comm>
2022-05-25 10:39:11 +0600

    banner section done

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,3 +1,4 @@
+import "animate.css";
 import React from 'react';
 import car from '../../../asset/Slider Car.png';
 
@@ -7,20 +8,18 @@ const Banner = () => {
     return (
         <section className="bg-hero-bg bg-center">
             <div className="container mx-auto">
-                <div className="flex flex-col md:flex-row justify-center items-center h-screen">
-                    <div className="text-white text-center md:text-left">
+                <div className="flex flex-col md:flex-row justify-between items-center h-screen">
+                    <div className="text-white text-center md:text-left animate__animated animate__fadeInTopLeft">
                         <h1 className="text-5xl my-6">
                             Auto Parts Online Store
                         </h1>
                         <p className="my-4">
                             Special offer for auto parts for electric cars and
-                            hybrid vehicles. To select the spare part you need,
-                            use the filter presented above or the advanced
-                            filter.
+                            hybrid vehicles. To select the spare part you need.
                         </p>
-                        <button class="btn btn-secondary my-4">Button</button>
+                        <button class="btn btn-primary my-4">Buy Now</button>
                     </div>
-                    <div class="ml-0 md:ml-64 transition ease-in-out">
+                    <div class="w-11/12 animate__animated animate__fadeInRightBig">
                         <img src={car} alt="" />
                     </div>
                 </div>
