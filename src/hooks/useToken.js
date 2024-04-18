018fd346de4db931fdd47c8fe73e64df4ac616c9
Md . Mahfujar Rahman <96248020+mdmahfujarrahman@users.noreply.github.com>
2024-04-18 22:12:15 +0600

    new asdded

--- src/hooks/useToken.js
+++ src/hooks/useToken.js
@@ -7,7 +7,13 @@ const useToken = user =>{
         const email = user?.user?.email
         const currentUser = {email: email}
         if(email){
-            fetch(`https://ahmed-auto-parts.vercel.app/user/${email}`)
+            fetch(`https://ahmed-auto-parts.vercel.app/user/${email}`, {
+                method: 'PUT',
+                headers: {
+                    "Content-type": 'application/json',
+                },
+                body: JSON.stringify(currentUser)
+            })
             .then(res => res.json())
             .then(data => {
                 const accessToken = data.token;
